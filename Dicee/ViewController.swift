cea81a42595223b1fa0fc3cdac53bc24c0b0849c
Sai Emani <Sai.Emani@docusign.com>
2019-05-29 10:20:14 -0700

    Dicee logic implemented.

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,16 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let diceImageArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
-
+    
     @IBAction func rollButtonPressed(_ sender: Any) {
+        updateDiceImages()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
+    fileprivate func updateDiceImages() {
+        let diceImageIndex1 = Int.random(in: 0...5)
+        let diceImageIndex2 = Int.random(in: 0...5)
+        
+        diceImageView1.image = UIImage(named: diceImageArray[diceImageIndex1])
+        diceImageView2.image = UIImage(named: diceImageArray[diceImageIndex2])
     }
     
 }
